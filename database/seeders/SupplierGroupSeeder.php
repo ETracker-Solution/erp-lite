331b3e0fde205fba320db0c343c0751250dfca92
Mohammad Ali <mdali89a@gmail.com>
2024-05-08 11:31:08 +0600

    Supplier Group seeder done

--- database/seeders/SupplierGroupSeeder.php
+++ database/seeders/SupplierGroupSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SupplierGroup;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class SupplierGroupSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        SupplierGroup::create( [
+            'id'=>1,
+            'name'=>'General',
+            'code'=>'001',
+            'created_at'=>'2023-06-07 13:01:13',
+            'updated_at'=>'2023-06-07 13:01:13'
+        ] );
+        SupplierGroup::create( [
+            'id'=>2,
+            'name'=>'Raw Material',
+            'code'=>'002',
+            'created_at'=>'2023-06-07 13:01:13',
+            'updated_at'=>'2023-06-07 13:01:13'
+        ] );
+        SupplierGroup::create( [
+            'id'=>3,
+            'name'=>'Packing Material',
+            'code'=>'001',
+            'created_at'=>'2023-06-07 13:01:13',
+            'updated_at'=>'2023-06-07 13:01:13'
+        ] );
     }
 }
