e51706dc9f29d3f6fe956e5f1d0224693ce4060e
s0hel-rana <sohelrananull@gmail.com>
2024-05-28 16:07:34 +0600

    error fixed

--- resources/views/requisition/action.blade.php
+++ resources/views/requisition/action.blade.php
@@ -2,11 +2,11 @@
     <form action="{{route('requisitions.destroy', $row->id)}}" method="post">
         <input type="hidden" name="_method" value="DELETE">
         @csrf
-        <a href="{{ route('requisitions.edit', $row->id) }}" class="btn btn-info btn-xs">
+        {{-- <a href="{{ route('requisitions.edit', $row->id) }}" class="btn btn-info btn-xs">
             <i class="fas fa-pencil-alt">
             </i>
             Edit
-        </a>
+        </a> --}}
         <a href="{{ route('requisitions.show', encrypt($row->id)) }}" class="btn btn-xs btn-primary">
             <i class="fas fa-folder">
             </i> Show
