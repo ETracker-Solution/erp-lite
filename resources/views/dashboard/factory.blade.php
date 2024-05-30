de565fb3a2ebf428344bacbe5df5ba03c54806f9
Mohammad Ali <mdali89a@gmail.com>
2024-05-30 12:31:54 +0600

    factory modify

--- resources/views/dashboard/factory.blade.php
+++ resources/views/dashboard/factory.blade.php
@@ -290,10 +290,10 @@
                                 @foreach($monthlyRequisitions as $row)
                                     <tr>
                                         <td>{{$loop->iteration}}</td>
-                                        <td> {{$row->outlet->name}}</td>
+                                        <td> {{$row->outlet->name ?? ''}}</td>
                                         <td>{!! showStatus($row->status) !!}</td>
                                         <td>{{$row->created_at->format('d-m-Y')}}</td>
-                                        <td><a target="_blank" href="{{ route('factory.finish-requisitions.show',encrypt($row->id))}}" title="View">
+                                        <td><a target="_blank" href="" title="View">
                                                 <i class="fas fa-eye ml-1"></i>
                                             </a></td>
                                     </tr>
