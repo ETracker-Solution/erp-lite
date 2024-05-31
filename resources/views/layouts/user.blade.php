f557b553dc039722ce77ade0fa9671b70280cd1c
Mohammad Ali <mdali89a@gmail.com>
2024-05-31 19:23:11 +0600

    app modify

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -22,7 +22,7 @@
 <body class="hold-transition register-page">
     <div class="register-box">
         <div class="register-logo">
-            <a href="#"><b>Admin</b>LTE</a>
+            <a href="#"><b>ERP</b>LITE</a>
         </div>
         @yield('content')
 
