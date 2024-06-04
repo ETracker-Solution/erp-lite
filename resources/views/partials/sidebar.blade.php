d1c91ffa4b12cddb416c3ebf195d0f84b17a88b2
Mohammad Ali <mdali89a@gmail.com>
2024-06-04 10:01:06 +0600

    modify sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{url('/dashboard')}}" class="brand-link">
-        <img src="{{ asset('upload').'/'.getSettingValue('company_logo') }}" alt="AdminLTE Logo"
+        <img src="{{ asset('upload').'/'.getSettingValue('company_logo') }}" alt="Logo"
              class="brand-image img-circle elevation-3" style="opacity: .8; margin-top: 1px;">
         <span class="brand-text font-weight-light">CakeTown</span>
     </a>
