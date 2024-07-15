2e76391772114825e19ca1dcf35fb209865550e0
s0hel-rana <sohelrananull@gmail.com>
2024-07-15 17:15:49 +0600

    batch action modify

--- resources/views/batch/action.blade.php
+++ resources/views/batch/action.blade.php
@@ -2,16 +2,19 @@
     <form action="{{route('batches.destroy', encrypt($row->id))}}" method="post">
         <input type="hidden" name="_method" value="DELETE">
         @csrf
-        <a href="{{ route('batches.edit',encrypt($row->id))}}" class="btn btn-info btn-xs">
-            <i class="fas fa-pencil-alt">
-            </i>
-            Edit
-        </a>
-        {{-- <a href="{{ route('batches.show', encrypt($row->id)) }}" class="btn btn-xs btn-primary">
-            <i class="fas fa-folder">
-            </i> Show
-        </a> --}}
-        <button id="btnDelete" class="btn btn-danger btn-xs"> <i class="fas fa-trash">
-            </i> Delete</button>
+        @if($row->is_consumption != 1 && $row->is_production != 1)
+            <a href="{{ route('batches.edit',encrypt($row->id))}}" class="btn btn-info btn-xs">
+                <i class="fas fa-pencil-alt">
+                </i>
+                Edit
+            </a>
+            {{-- <a href="{{ route('batches.show', encrypt($row->id)) }}" class="btn btn-xs btn-primary">
+                <i class="fas fa-folder">
+                </i> Show
+            </a> --}}
+            <button id="btnDelete" class="btn btn-danger btn-xs"> <i class="fas fa-trash">
+                </i> Delete</button>
+        @endif
+        
     </form>
 </div>
