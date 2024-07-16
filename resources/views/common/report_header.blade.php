2a99884bad41f4c19eb862f31c262d4bbf8be597
s0hel-rana <sohelrananull@gmail.com>
2024-07-16 15:24:39 +0600

    report address issue fixed

--- resources/views/common/report_header.blade.php
+++ resources/views/common/report_header.blade.php
@@ -1,8 +1,8 @@
 <tr>
     <td class="w-full headers">
-        <h3>CAKE TOWN BD</h3>
-        <p>
-            157 Distrilary Road, Gandaria, Dhaka, Bangladesh
-        </p>
+        <h3>{{ getSettingValue('company_name') }}</h3>
+        <p> Address : {{ getSettingValue('company_address') }}</p>
+        <p> Email : {{ getSettingValue('company_email') }}</p>
+        <p> Phone : {{ getSettingValue('company_phone') }}</p>
     </td>
 </tr>
