09d4838606d167573f8d90a6dbf23fc2d109e1c4
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2024-10-24 12:18:44 +0600

    fg requisition issue fix

--- resources/views/requisition/create.blade.php
+++ resources/views/requisition/create.blade.php
@@ -396,7 +396,18 @@
                                        // vm.selected_items=[];
                                         vm.item_id= '';
                                         let items = response.data.products;
+
                                         for (let key in items) {
+                                            let exists = vm.selected_items.some(function (field) {
+                                                return field.id == items[key].id
+                                            });
+                                            if (exists){
+                                                toastr.error('Item Already Selected Fom this group', {
+                                                    closeButton: true,
+                                                    progressBar: true,
+                                                });
+                                                return
+                                            }
                                             vm.selected_items.push(items[key]);
                                         }
                                         console.log(vm.selected_items);
