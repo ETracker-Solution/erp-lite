af80eaf5023beebc6cded303c149df8cc2797bf2
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2024-10-31 12:32:35 +0600

    remove customer delete option

--- resources/views/customer/action.blade.php
+++ resources/views/customer/action.blade.php
@@ -1,17 +1,11 @@
 <div class="project-actions text-right">
-    <form action="{{route('customers.destroy', encrypt($row->id))}}" method="post">
-        <input type="hidden" name="_method" value="DELETE">
-        @csrf
-        <a href="{{ route('customers.edit', encrypt($row->id)) }}" class="btn btn-info btn-xs">
-            <i class="fas fa-pencil-alt">
-            </i>
-            Edit
-        </a>
-        <a href="{{ route('customers.show', encrypt($row->id)) }}" class="btn btn-xs btn-primary">
-            <i class="fas fa-folder">
-            </i> Show
-        </a>
-        <button id="btnDelete" class="btn btn-danger btn-xs"> <i class="fas fa-trash">
-            </i> Delete</button>
-    </form>
+    <a href="{{ route('customers.edit', encrypt($row->id)) }}" class="btn btn-info btn-xs">
+        <i class="fas fa-pencil-alt">
+        </i>
+        Edit
+    </a>
+    <a href="{{ route('customers.show', encrypt($row->id)) }}" class="btn btn-xs btn-primary">
+        <i class="fas fa-folder">
+        </i> Show
+    </a>
 </div>
