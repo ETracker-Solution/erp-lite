ffc4dc03317dbaf605edd88d05013729f1560251
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2024-10-31 12:24:09 +0600

    ide petty cash from outlet ft voucher

--- app/Http/Controllers/FundTransferVoucherController.php
+++ app/Http/Controllers/FundTransferVoucherController.php
@@ -72,9 +72,9 @@ class FundTransferVoucherController extends Controller
             //     foreach ($cons as $con) {
             //         $chartOfAccounts[] = $con->coa;
             //     }
-            $chartOfAccounts = OutletAccount::with(['coa'=>function ($q) {
+            $chartOfAccounts = OutletAccount::with(['coa'])->whereHas('coa',function ($q) {
                 $q->where('default_type', '!=','petty_cash');
-            }])->where('outlet_id', \auth()->user()->employee->outlet_id)->get();
+            })->where('outlet_id', \auth()->user()->employee->outlet_id)->get();
             $toChartOfAccounts = ChartOfAccount::where(['default_type' => 'office_account', 'is_bank_cash' => 'yes', 'type' => 'ledger', 'status' => 'active'])->get();
 
         } else {
