28b255a3a3249fe492ea0eb212bd0d7bbb0295a5
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2024-11-28 12:22:17 +0600

    todays req report issue fix

--- resources/views/common/report_header.blade.php
+++ resources/views/common/report_header.blade.php
@@ -1,9 +1,12 @@
 <tr>
-    <td class="w-full headers">
+    <td class="w-full headers" style="border: unset !important;">
         <img height="80px" width="300px" src="{{ public_path('upload'.'/'.getSettingValue('company_logo')) }}" alt=""
-    style="height: 150px;">
+             style="height: 150px;">
         <p> Address : {{ getSettingValue('company_address') }}</p>
         <p> Email : {{ getSettingValue('company_email') }}</p>
         <p> Phone : {{ getSettingValue('company_phone') }}</p>
+        @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'today.requisitions.export')
+            <p> User Name : {{ auth()->user()->name }}</p>
+        @endif
     </td>
 </tr>
--- resources/views/exports/todays_requisition.blade.php
+++ resources/views/exports/todays_requisition.blade.php
@@ -3,10 +3,47 @@
         border: 1px solid black;
         border-collapse: collapse;
     }
+
     th {
         background-color: #c4c7c7;
     }
+
+    .headers {
+        text-align: center
+    }
+
+    .signature-section {
+        margin-top: 50px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .signature-section div {
+        text-align: center;
+        font-weight: bold;
+        width: 32%;
+    }
 </style>
+
+@if(\Illuminate\Support\Facades\Route::currentRouteName() == 'today.requisitions.export')
+    <table style="width: 100% ; border: unset !important;">
+        <tbody>
+        @include('common.report_header')
+        </tbody>
+    </table>
+    <table style="margin-top: 70px; width:100%; text-align: center !important;border: unset !important;">
+        <tr style="text-align: center !important;border: unset !important;">
+            <td style="border: unset !important;"><strong
+                    style="border-top: 1px solid black;text-align: end !important;">Prepared
+                    By</strong></td>
+            <td style="border: unset !important;"><strong
+                    style="border-top: 1px solid black;text-align: center !important; ">Production
+                    Manager/Staff</strong>
+            </td>
+        </tr>
+    </table>
+    <hr>
+@endif
 <table style="width: 100%">
     <thead>
     <tr>
