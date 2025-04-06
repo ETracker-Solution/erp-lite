84666b514a5ddca11df9f603fc0d0dfc6015c46e
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2025-04-06 09:30:45 +0600

    memory issue fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -290,6 +290,8 @@ class ApiController extends Controller
 
     public function fetchRequisitionById($id, $store_id = null)
     {
+        ini_set('memory_limit', '256M');
+
         $requisition = Requisition::with(['items.coi.unit', 'items.coi.parent', 'deliveries.items', 'items.coi.requisitionDeliveryItems.requisitionDelivery', 'items.coi.preOrderItems.preOrder'])
             ->where('id', $id)
             ->firstOrFail();
