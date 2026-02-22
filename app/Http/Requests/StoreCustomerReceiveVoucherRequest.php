ba5afd18b2c767c0849a2166d12fbdbbee28e243
shamimxp <shamim.dev2@gmail.com>
2026-02-22 15:18:41 +0600

    add column in sale settle discount

--- app/Http/Requests/StoreCustomerReceiveVoucherRequest.php
+++ app/Http/Requests/StoreCustomerReceiveVoucherRequest.php
@@ -25,16 +25,17 @@ class StoreCustomerReceiveVoucherRequest extends FormRequest
             'date' => 'required',
             'products' => 'array',
             'products.*.amount' => 'required',
+            'products.*.settle_discount' => 'nullable',
             'products.*.customer_id' => 'required',
             'products.*.sale_id' => 'required', // Invoice ID
             'products.*.debit_account_id' => ['required'], // Receipt Account (Bank/Cash)
-            // Credit Account would be Accounts Receivable or Customer? 
+            // Credit Account would be Accounts Receivable or Customer?
             // In Accounting: Debit Cash/Bank, Credit Receivables (Customer)
             // So we need debit_account_id (received to)
             'narration' => 'nullable',
         ];
     }
-    
+
     public function prepareForValidation()
     {
         //
