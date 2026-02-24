6836692e81f42a870a1a50116ea678b3f932a45d
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2026-02-24 09:56:06 +0600

    sale cancel issue fix

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use niklasravnsborg\LaravelPdf\Facades\Pdf;
 use stdClass;
 use Yajra\DataTables\Facades\DataTables;
