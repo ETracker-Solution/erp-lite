de103284fd159eac6d435a0379e95518ed0f0c92
shamimxp <shamim.dev2@gmail.com>
2026-03-08 12:38:15 +0600

    add filter last

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -20,20 +20,56 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        $employees = Employee::all();
         if (\request()->ajax()) {
+            $employees = Employee::query();
+            $employees = $this->filter($employees);
+
             return DataTables::of($employees)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     return view('employee.action', compact('row'));
                 })
                 ->addColumn('created_at', function ($row) {
-                    return view('common.created_at',compact('row'));
+                    return view('common.created_at', compact('row'));
                 })
-                ->rawColumns(['action','created_at'])
+                ->rawColumns(['action', 'created_at'])
                 ->make(true);
         }
         return view('employee.index');
+    }
+
+    protected function filter($data)
+    {
+        if (request('date_range')) {
+            $dateRange = [];
+            if (str_contains(request('date_range'), ' to ')) {
+                $dateRange = explode(' to ', request('date_range'));
+            } elseif (str_contains(request('date_range'), ' - ')) {
+                $dateRange = explode(' - ', request('date_range'));
+            } else {
+                $dateRange = [request('date_range'), request('date_range')];
+            }
+
+            if (isset($dateRange[0]) && isset($dateRange[1])) {
+                $data->whereBetween('created_at', [$dateRange[0] . ' 00:00:00', $dateRange[1] . ' 23:59:59']);
+            } elseif (isset($dateRange[0])) {
+                $data->whereDate('created_at', $dateRange[0]);
+            }
+        }
+
+        if (request()->filled('name')) {
+            $data->where('name', 'like', '%' . request('name') . '%');
+        }
+
+        if (request()->filled('email')) {
+            $data->where('email', 'like', '%' . request('email') . '%');
+        }
+
+        if (request()->filled('phone')) {
+            $data->where('phone', 'like', '%' . request('phone') . '%');
+        }
+
+        return $data->latest();
     }
 
     /**
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -21,6 +21,39 @@ Employee List
 
         <div class="row">
             <div class="col-12">
+                <div class="row align-items-end mb-3">
+                    <div class="col-md-3">
+                        <div class="form-group mb-0">
+                            <label for="date_range">Date Range</label>
+                            <input type="text" id="date_range" class="form-control filter-input flatpickr-range" placeholder="YYYY-MM-DD to YYYY-MM-DD">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group mb-0">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control filter-input" placeholder="Employee Name">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="form-group mb-0">
+                            <label for="email">Email</label>
+                            <input type="text" id="email" class="form-control filter-input" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="form-group mb-0">
+                            <label for="phone">Phone</label>
+                            <input type="text" id="phone" class="form-control filter-input" placeholder="Phone No">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                         <div class="form-group mb-0">
+                            <button type="button" id="reset-btn" class="btn btn-secondary btn-block">Reset</button>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">Employee List</h3>
@@ -49,6 +82,8 @@ Employee List
 @section('css')
     <!-- DataTables -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('datepicker/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('datepicker/app-assets/css/plugins/forms/pickers/form-flat-pickr.css') }}">
 @endsection
 @section('js')
 <!-- DataTables -->
@@ -56,11 +91,29 @@ Employee List
 <script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('datepicker/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
 @endsection
 @push('script')
 <!-- page script -->
 <script>
+    function recallDatatable() {
+        $('#dataTable').DataTable().draw(true);
+    }
+
     $(document).ready(function() {
+        if (sessionStorage.getItem('date_range_employee')) {
+            $('#date_range').val(sessionStorage.getItem('date_range_employee'));
+        }
+        if (sessionStorage.getItem('name_employee')) {
+            $('#name').val(sessionStorage.getItem('name_employee'));
+        }
+        if (sessionStorage.getItem('email_employee')) {
+            $('#email').val(sessionStorage.getItem('email_employee'));
+        }
+        if (sessionStorage.getItem('phone_employee')) {
+            $('#phone').val(sessionStorage.getItem('phone_employee'));
+        }
+
         $('#dataTable').dataTable({
             stateSave: true,
             responsive: true,
@@ -68,6 +121,12 @@ Employee List
             processing: true,
             ajax: {
                 url: "{{ route('employees.index') }}",
+                data: function (d) {
+                    d.date_range = $('#date_range').val();
+                    d.name = $('#name').val();
+                    d.email = $('#email').val();
+                    d.phone = $('#phone').val();
+                }
             },
             columns: [{
                 data: "DT_RowIndex",
@@ -79,31 +138,37 @@ Employee List
                 {
                     data: "employee_id",
                     title: "Employee ID",
+                    name: "employee_id",
                     searchable: true
                 },
                 {
                     data: "name",
                     title: "Name",
+                    name: "name",
                     searchable: true
                 },
                 {
                     data: "email",
                     title: "email",
+                    name: "email",
                     searchable: true
                 },
                 {
                     data: "phone",
                     title: "Phone",
+                    name: "phone",
                     searchable: true
                 },
                 {
                     data: "present_address",
                     title: "Present Address",
+                    name: "present_address",
                     searchable: true
                 },
                 {
                     data: "created_at",
                     title: "created at",
+                    name: "created_at",
                     searchable: true
                 },
                 {
@@ -113,6 +178,31 @@ Employee List
                     searchable: false
                 },
             ],
+        });
+
+        $('.flatpickr-range').flatpickr({
+            mode: "range",
+            dateFormat: "Y-m-d",
+        });
+
+        $('.filter-input').on('keyup change', function () {
+            sessionStorage.setItem('date_range_employee', $('#date_range').val());
+            sessionStorage.setItem('name_employee', $('#name').val());
+            sessionStorage.setItem('email_employee', $('#email').val());
+            sessionStorage.setItem('phone_employee', $('#phone').val());
+            recallDatatable();
+        });
+
+        $('#reset-btn').on('click', function () {
+            $('.filter-input').val('');
+            if ($('#date_range').length > 0 && $('#date_range')[0]._flatpickr) {
+                $('#date_range')[0]._flatpickr.clear();
+            }
+            sessionStorage.removeItem('date_range_employee');
+            sessionStorage.removeItem('name_employee');
+            sessionStorage.removeItem('email_employee');
+            sessionStorage.removeItem('phone_employee');
+            recallDatatable();
         });
     })
 </script>
