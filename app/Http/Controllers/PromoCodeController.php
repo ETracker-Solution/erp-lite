cb2a82e7b1f1d0a51e619cc5ae0e26056a9d525b
shamimxp <shamim.dev2@gmail.com>
2026-03-08 09:59:38 +0600

    add filter

--- app/Http/Controllers/PromoCodeController.php
+++ app/Http/Controllers/PromoCodeController.php
@@ -24,7 +24,9 @@ class PromoCodeController extends Controller
     public function index()
     {
         if (\request()->ajax()) {
-            $codes = PromoCode::latest();
+            $codes = PromoCode::query();
+            $codes = $this->filter($codes);
+
             return DataTables::of($codes)
                 ->addIndexColumn()
                 ->editColumn('start_date', function ($row) {
@@ -46,6 +48,32 @@ class PromoCodeController extends Controller
                 ->make(true);
         }
         return view('promo_code.index');
+    }
+
+    protected function filter($data)
+    {
+        if (request('date_range')) {
+            $dateRange = [];
+            if (str_contains(request('date_range'), ' to ')) {
+                $dateRange = explode(' to ', request('date_range'));
+            } elseif (str_contains(request('date_range'), ' - ')) {
+                $dateRange = explode(' - ', request('date_range'));
+            } else {
+                $dateRange = [request('date_range'), request('date_range')];
+            }
+
+            if (isset($dateRange[0]) && isset($dateRange[1])) {
+                $data->whereBetween('created_at', [$dateRange[0] . ' 00:00:00', $dateRange[1] . ' 23:59:59']);
+            } elseif (isset($dateRange[0])) {
+                $data->whereDate('created_at', $dateRange[0]);
+            }
+        }
+
+        if (request()->filled('code')) {
+            $data->where('code', 'like', '%' . request('code') . '%');
+        }
+
+        return $data->latest();
     }
 
     /**
--- resources/views/promo_code/index.blade.php
+++ resources/views/promo_code/index.blade.php
@@ -16,6 +16,27 @@ $links = [
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                <div class="row align-items-end mb-3">
+                    <div class="col-md-4">
+                        <div class="form-group mb-0">
+                            <label for="date_range">Date Range</label>
+                            <input type="text" id="date_range" class="form-control filter-input flatpickr-range" placeholder="YYYY-MM-DD to YYYY-MM-DD">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group mb-0">
+                            <label for="code">Code</label>
+                            <input type="text" id="code" class="form-control filter-input" placeholder="Promo Code">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                         <div class="form-group mb-0">
+                            <button type="button" id="reset-btn" class="btn btn-secondary btn-block">Reset</button>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">All Promo Code List</h3>
@@ -44,6 +65,8 @@ $links = [
 @section('css')
 <!-- DataTables -->
 <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('datepicker/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('datepicker/app-assets/css/plugins/forms/pickers/form-flat-pickr.css') }}">
 @endsection
 @push('style')
 
@@ -54,10 +77,22 @@ $links = [
 <script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('datepicker/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
 @endsection
 @push('script')
 <script>
+    function recallDatatable() {
+        $('#dataTable').DataTable().draw(true);
+    }
+
     $(document).ready(function () {
+            if (sessionStorage.getItem('date_range_promo')) {
+                $('#date_range').val(sessionStorage.getItem('date_range_promo'));
+            }
+            if (sessionStorage.getItem('code_promo')) {
+                $('#code').val(sessionStorage.getItem('code_promo'));
+            }
+
             $('#dataTable').dataTable({
                 stateSave: true,
                 responsive: true,
@@ -65,6 +100,10 @@ $links = [
                 processing: true,
                 ajax: {
                     url: "{{ route('promo-codes.index') }}",
+                    data: function (d) {
+                        d.date_range = $('#date_range').val();
+                        d.code = $('#code').val();
+                    }
                 },
                 columns: [
                     {
@@ -77,29 +116,34 @@ $links = [
                     {
                         data: "code",
                         title: "code",
+                        name: "code",
                         searchable: true
                     },
                     {
                         data: "start_date",
                         title: "start date",
+                        name: "start_date",
                         orderable: false,
                         "defaultContent": "Not Set"
                     },
                     {
                         data: "end_date",
                         title: "end date",
+                        name: "end_date",
                         searchable: false,
                         orderable: false,
                     },
                     {
                         data: "discount",
                         title: "Discount",
+                        name: "discount",
                         searchable: false,
                         orderable: false,
                     },
                     {
                         data: "created_at",
                         title: "created at",
+                        name: "created_at",
                         searchable: true
                     },
                     {
@@ -109,6 +153,27 @@ $links = [
                         searchable: false
                     },
                 ],
+            });
+
+            $('.flatpickr-range').flatpickr({
+                mode: "range",
+                dateFormat: "Y-m-d",
+            });
+
+            $('.filter-input').on('keyup change', function () {
+                sessionStorage.setItem('date_range_promo', $('#date_range').val());
+                sessionStorage.setItem('code_promo', $('#code').val());
+                recallDatatable();
+            });
+
+            $('#reset-btn').on('click', function () {
+                $('.filter-input').val('');
+                if ($('#date_range').length > 0 && $('#date_range')[0]._flatpickr) {
+                    $('#date_range')[0]._flatpickr.clear();
+                }
+                sessionStorage.removeItem('date_range_promo');
+                sessionStorage.removeItem('code_promo');
+                recallDatatable();
             });
         })
 </script>
