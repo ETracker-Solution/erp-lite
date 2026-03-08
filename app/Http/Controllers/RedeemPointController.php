79dbbb6a50367d596553159482d2cb33e1b5bf01
shamimxp <shamim.dev2@gmail.com>
2026-03-08 09:13:41 +0600

    add filter

--- app/Http/Controllers/RedeemPointController.php
+++ app/Http/Controllers/RedeemPointController.php
@@ -18,19 +18,55 @@ class RedeemPointController extends Controller
     public function index()
     {
         if (\request()->ajax()) {
-            $redeemPoints = MembershipPointHistory::with('customer','sale')->where('point','<',0)->latest();
+            $redeemPoints = MembershipPointHistory::with('customer', 'sale')->where('point', '<', 0);
+            $redeemPoints = $this->filter($redeemPoints);
+
             return DataTables::of($redeemPoints)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     return view('redeem-point.action-button', compact('row'));
                 })
                 ->addColumn('created_at', function ($row) {
-                    return view('common.created_at',compact('row'));
+                    return view('common.created_at', compact('row'));
                 })
-                ->rawColumns(['action','created_at'])
+                ->rawColumns(['action', 'created_at'])
                 ->make(true);
         }
         return view('redeem-point.index');
+    }
+
+    protected function filter($data)
+    {
+        if (request('date_range')) {
+            $dateRange = [];
+            if (str_contains(request('date_range'), ' to ')) {
+                $dateRange = explode(' to ', request('date_range'));
+            } elseif (str_contains(request('date_range'), ' - ')) {
+                $dateRange = explode(' - ', request('date_range'));
+            } else {
+                $dateRange = [request('date_range'), request('date_range')];
+            }
+
+            if (isset($dateRange[0]) && isset($dateRange[1])) {
+                $data->whereBetween('created_at', [$dateRange[0] . ' 00:00:00', $dateRange[1] . ' 23:59:59']);
+            } elseif (isset($dateRange[0])) {
+                $data->whereDate('created_at', $dateRange[0]);
+            }
+        }
+
+        if (request()->filled('customer_name')) {
+            $data->whereHas('customer', function ($query) {
+                $query->where('name', 'like', '%' . request('customer_name') . '%');
+            });
+        }
+
+        if (request()->filled('invoice_no')) {
+            $data->whereHas('sale', function ($query) {
+                $query->where('invoice_number', 'like', '%' . request('invoice_no') . '%');
+            });
+        }
+
+        return $data->latest();
     }
 
     /**
--- resources/views/redeem-point/index.blade.php
+++ resources/views/redeem-point/index.blade.php
@@ -2,13 +2,10 @@
 
 @section('title', 'Redeem Point List')
 @push('style')
-<link rel="stylesheet" type="text/css" href="{{asset('admin/app-assets/vendors/css/pickers/pickadate/pickadate.css')}}">
 <link rel="stylesheet" type="text/css"
-    href="{{asset('admin/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css')}}">
+    href="{{asset('datepicker/app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css')}}">
 <link rel="stylesheet" type="text/css"
-    href="{{asset('admin/app-assets/css/plugins/forms/pickers/form-flat-pickr.css')}}">
-<link rel="stylesheet" type="text/css"
-    href="{{asset('admin/app-assets/css/plugins/forms/pickers/form-pickadate.css')}}">
+    href="{{asset('datepicker/app-assets/css/plugins/forms/pickers/form-flat-pickr.css')}}">
 @endpush
 @section('content')
 @php
@@ -24,13 +21,36 @@ $links = [
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                <div class="row align-items-end mb-3">
+                    <div class="col-md-3">
+                        <div class="form-group mb-0">
+                            <label for="date_range">Date Range</label>
+                            <input type="text" id="date_range" class="form-control filter-input flatpickr-range" placeholder="YYYY-MM-DD to YYYY-MM-DD">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group mb-0">
+                            <label for="customer_name">Customer</label>
+                            <input type="text" id="customer_name" class="form-control filter-input" placeholder="Customer Name">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group mb-0">
+                            <label for="invoice_no">Invoice Number</label>
+                            <input type="text" id="invoice_no" class="form-control filter-input" placeholder="Invoice Number">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                         <div class="form-group mb-0">
+                            <button type="button" id="reset-btn" class="btn btn-secondary btn-block">Reset</button>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">All Redeem Point List</h3>
-                        {{-- <div class="card-tools">
-                            <a href="#"><button class="btn btn-sm btn-primary"><i class="fa fa-plus-circle"
-                                        aria-hidden="true"></i> &nbsp;Add New</button></a>
-                        </div> --}}
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body table-responsive">
@@ -62,10 +82,25 @@ $links = [
 <script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
 <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('datepicker/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script>
 @endsection
 @push('script')
 <script>
+    function recallDatatable() {
+        $('#dataTable').DataTable().draw(true);
+    }
+
     $(document).ready(function () {
+            if (sessionStorage.getItem('date_range_redeem')) {
+                $('#date_range').val(sessionStorage.getItem('date_range_redeem'));
+            }
+            if (sessionStorage.getItem('customer_name_redeem')) {
+                $('#customer_name').val(sessionStorage.getItem('customer_name_redeem'));
+            }
+            if (sessionStorage.getItem('invoice_no_redeem')) {
+                $('#invoice_no').val(sessionStorage.getItem('invoice_no_redeem'));
+            }
+
             $('#dataTable').dataTable({
                 stateSave: true,
                 responsive: true,
@@ -73,6 +108,11 @@ $links = [
                 processing: true,
                 ajax: {
                     url: "{{ route('redeem-points.index') }}",
+                    data: function (d) {
+                        d.date_range = $('#date_range').val();
+                        d.customer_name = $('#customer_name').val();
+                        d.invoice_no = $('#invoice_no').val();
+                    }
                 },
                 columns: [{
                     data: "DT_RowIndex",
@@ -85,31 +125,52 @@ $links = [
                     {
                         data: "customer.name",
                         title: "customer",
+                        name: "customer.name",
                         searchable: true,
                         "defaultContent": "No Set"
                     },
                     {
                         data: "sale.invoice_number",
                         title: "Invoice Number",
+                        name: "sale.invoice_number",
                         searchable: true,
                         "defaultContent": "No Set"
                     }, {
                         data: "point",
                         title: "point",
+                        name: "point",
                         searchable: true
                     },
                     {
                         data: "created_at",
                         title: "created at",
+                        name: "created_at",
                         searchable: true
                     },
-                    // {
-                    //     data: "action",
-                    //     title: "Action",
-                    //     orderable: false,
-                    //     searchable: false
-                    // },
                 ],
+            });
+
+            $('.flatpickr-range').flatpickr({
+                mode: "range",
+                dateFormat: "Y-m-d",
+            });
+
+            $('.filter-input').on('keyup change', function () {
+                sessionStorage.setItem('date_range_redeem', $('#date_range').val());
+                sessionStorage.setItem('customer_name_redeem', $('#customer_name').val());
+                sessionStorage.setItem('invoice_no_redeem', $('#invoice_no').val());
+                recallDatatable();
+            });
+
+            $('#reset-btn').on('click', function () {
+                $('.filter-input').val('');
+                if ($('#date_range').length > 0 && $('#date_range')[0]._flatpickr) {
+                    $('#date_range')[0]._flatpickr.clear();
+                }
+                sessionStorage.removeItem('date_range_redeem');
+                sessionStorage.removeItem('customer_name_redeem');
+                sessionStorage.removeItem('invoice_no_redeem');
+                recallDatatable();
             });
         })
 </script>
